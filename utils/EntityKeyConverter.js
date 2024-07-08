a2f09d2a79586ce98a5b1495cc13af34ed31b8a4
badac <badaclock@gmail.com>
2024-07-08 09:31:58 +0900

    Feat: 근태리스트 CRUD API 구현

---
+++ utils/EntityKeyConverter.js
@@ -0,0 +1,31 @@
+import { camelToSnakeCase } from './camelToSnakeCase.js';
+import { snakeToCamelCase } from './snakeToCamelCase.js';
+
+export class EntityKeyConverter {
+  constructor() {}
+
+  // 객체의 키를 스네이크케이스로 변환
+  convertKeysToSnakeCase(keys) {
+    return keys.map(key => camelToSnakeCase(key));
+  }
+
+  // 필드(배열 안에 객체가 들어 있는 형태)를 스네이크케이스로 변환
+  convertFieldsToCamelCase(fields) {
+    return fields.map(field => {
+      const newField = {};
+      for (const key in field) {
+        newField[snakeToCamelCase(key)] = field[key];
+      }
+      return newField;
+    });
+  }
+
+  // 객체의 키를 카멜케이스로 변환
+  convertEntitySnakeToCamelCaseKeys(entity) {
+    const newEntity = {};
+    for (const key in entity) {
+      newEntity[snakeToCamelCase(key)] = entity[key];
+    }
+    return newEntity;
+  }
+}
--- utils/snakeToCamelCase.js
+++ utils/snakeToCamelCase.js
@@ -1,48 +1,9 @@
 // 스네이크케이스를 카멜케이스로 변환하는 함수
 // ex) snakeToCamelCase('login_id') => 'loginId'
 function snakeToCamelCase(str) {
-  return str.replace(/([-_][a-z])/g, (group) =>
-    group.toUpperCase().replace("-", "").replace("_", "")
+  return str.replace(/([-_][a-z])/g, group =>
+    group.toUpperCase().replace('-', '').replace('_', '')
   );
 }
 
-// const testArray = [
-//   {
-//     id: 1,
-//     login_id: "C2319007",
-//     password: "1q2w3e4r",
-//     name: "김낙연",
-//     email: "clock@gamil.com",
-//     position: "차장",
-//     phone: "010-2098-5620",
-//     img: "https://i.imgur.com/3R9iaYo.jpeg",
-//     is_deleted: 0,
-//   },
-// ];
-
-// const result = testArray.map((user) => {
-//   const newUser = {};
-//   for (const key in user) {
-//     newUser[snakeToCamelCase(key)] = user[key];
-//   }
-//   return newUser;
-// });
-// console.log(result);
-
-// const testObj = {
-//   id: 1,
-//   login_id: "C2319007",
-//   email: "clock@gamil.com",
-//   position: "차장",
-//   phone: "010-2098-5620",
-//   img: "https://i.imgur.com/3R9iaYo.jpeg",
-// };
-
-// const result = {};
-// for (const key in testObj) {
-//   result[snakeToCamelCase(key)] = testObj[key];
-// }
-// console.log("result", result);
-
-// console.log(snakeToCamelCase("loginId"));
 export { snakeToCamelCase };
